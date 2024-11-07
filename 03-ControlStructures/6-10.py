4c0516b73d93082389e8c9ed2921eb388ec12e30
Sakovskyi Danylo <danylosakovskyi@gmail.com>
2024-11-08 00:04:05 +0100

    6 part

---
+++ 03-ControlStructures/6-10.py
@@ -0,0 +1,23 @@
+###
+# String manipulation
+#
+
+movie = "The Lord of the Rings: The Return of the King"
+
+# Print number of characters
+print('Number of characters:', len(movie))
+
+# Print title in capital letters
+print('Title in capital letters:', movie.upper())
+
+# Print title in small letters
+print('Title in small letters:', movie.lower())
+
+# Print how many times the vowel "e" appears in the title
+print('Number of times "e" appears:', movie.count('e'))
+
+# Print where in the text is the word "Lord"
+print('Position of "Lord":', movie.find('Lord'))
+
+# Print where in the text is the word "dragon"
+print('Position of "dragon":', movie.find('dragon'))
---
+++ 03-ControlStructures/6-3.py
@@ -0,0 +1,7 @@
+
+university = "Krakow University of Economics"
+
+
+abbreviation = "".join(word[0] for word in university.split())
+
+print("Abbreviation:", abbreviation)

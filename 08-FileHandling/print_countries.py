6c4d4a7f37719e54fecfc847691f50069ae9f496
Danylo Sakovskyi <danylosakovskyi@gmail.com>
2024-11-29 12:23:23 +0100

    0

--- 08-FileHandling/print_countries.py
+++ 08-FileHandling/print_countries.py
@@ -1,6 +1,7 @@
 ###
 # Reads from file, line by line
-#
+i=1
 with open('countries.txt', 'r') as file:
     for line in file:
-        print(line, end="")
+        print(f'{i} {line}')
+        i+=1
